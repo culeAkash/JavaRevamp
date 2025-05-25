1bf792c6fa3d03698a430b89ed1812052f9afc1c
Akash Jaiswal <akashjais929@gmail.com>
2025-05-25 23:42:14 +0530

    Used and understood HashMap implementation and hashCode and equals method

--- src/collection/framework/maps/HashMapDemo.java
+++ src/collection/framework/maps/HashMapDemo.java
@@ -2,11 +2,12 @@ package collection.framework.maps;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 
 public class HashMapDemo {
     public static void main(String[] args) {
-        Map<Integer,String> map = new HashMap<>();
+        HashMap<Integer,String> map = new HashMap<>();
 
         map.put(1,"Akash");
         map.put(2,"Amit");
@@ -34,5 +35,75 @@ public class HashMapDemo {
             System.out.println(entry.getKey() + " -> " + entry.getValue());
         }
 
+    }
+}
+
+class HashMapExample{
+    public static void main(String[] args) {
+        Map<Student,Integer> map = new HashMap<>();
+
+
+        // Since default hashCode() in Object class works by doing some operations on the memory location, two seperate objects with
+        // same values will be treated as seperate keys, so we have to implement our own hashCode method
+
+        // even if the hashcode is same, both keys are matched based on the equals(). So, implement and equals() also.
+
+        Student s1 = new Student("Akash",1);
+        Student s2 = new Student("Amit",2);
+        Student s3 = new Student("Akash",1);
+
+        map.put(s1,90);
+        map.put(s2,88);
+        map.put(s3,99);
+
+        System.out.println(map.get(s1) + " " + map.get(s3));
+        System.out.println(map.size());
+
+
+    }
+}
+
+class Student{
+    private String name;
+    private int rollNumber;
+
+    public Student(String name, int rollNumber) {
+        this.name = name;
+        this.rollNumber = rollNumber;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public int getRollNumber(){
+        return this.rollNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", rollNumber=" + rollNumber +
+                '}';
+    }
+
+    @Override
+    public int hashCode() {
+        // calculates hash based on elements and not reference
+        return Objects.hash(this.name,this.rollNumber);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj==this)return true;
+
+        if(obj==null)return false;
+
+        if(obj.getClass() != getClass())return false;
+
+        Student other = (Student) obj;
+
+        return Objects.equals(other.name,this.name) && other.rollNumber == this.rollNumber;
     }
 }
---
+++ src/collection/framework/maps/LinkedHashMapDemo.java
@@ -0,0 +1,11 @@
+package collection.framework.maps;
+
+import java.util.LinkedHashMap;
+
+public class LinkedHashMapDemo {
+    public static void main(String[] args) {
+        LinkedHashMap<String,Integer> linkedHashMap = new LinkedHashMap<>();
+
+
+    }
+}
